876dbecdbf42fd3c5fd5a207dfea45b81e20e690
Amlakab <amlakieabebaw96@gmail.com>
2025-09-09 16:40:53 +0300

    loser model addedddddyooooopppppp

--- components/player/GameInterface.tsx
+++ components/player/GameInterface.tsx
@@ -1157,7 +1157,7 @@ const WinnerCard = ({ winner, isCurrentUser, language }: {
                 onClick={handleBackToLobbyWithRefund}
                 fullWidth
                 size="small"
-                sx={{ fontSize: '0.95rem', mt: 1, p: 2 }}
+                sx={{ fontSize: '0.95rem', mt: 1, p: 1 }}
               >
                 {language === 'am' ? 'ካችሮችን አጥፋ' : 'Clear Card'}
               </Button>
