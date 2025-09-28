a57fc9222b328bf942e6bf691136cd724a1d9dc3
Amlakab <amlakieabebaw96@gmail.com>
2025-09-29 00:48:56 +0300

    corected 28

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -17,7 +17,7 @@ export default function AdminLayout({ children }: AdminLayoutProps) {
   const closeSidebar = () => setSidebarOpen(false);
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div>
         <main className="flex-1 p-6 lg:ml-0">
           <div className="bg-white rounded-lg shadow-sm p-6">
             {children}
