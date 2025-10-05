799ae00202fb27e178b11af3446ed3783757e91a
Amlakab <amlakieabebaw96@gmail.com>
2025-10-05 15:07:40 +0300

    corected 67

--- components/player/BetSelectionPage.tsx
+++ components/player/BetSelectionPage.tsx
@@ -498,7 +498,7 @@ const BetSelectionPage = ({
           })}
         </Box>
 
-        {/* Footer */}
+        {/* Footerrrr */}
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
