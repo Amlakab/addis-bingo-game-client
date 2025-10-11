24af82494ecb9013b07d8d9e01935a9970bb92fa
Amlakie Abebaw <amlakieabebaw96@gmail.com>
2025-10-11 10:15:56 -0400

    corected code of player lobby

--- components/player/GameInterface.tsx
+++ components/player/GameInterface.tsx
@@ -606,7 +606,7 @@ const GameInterface = ({
 
   // UPDATED: Start countdown when server timer is available
   useEffect(() => {
-    if (countdown > 0 && !gameStarted && !gameStopped) {
+    if (!gameStarted && !gameStopped) {
       if(countdown===4 || countdown===3 || countdown===2 || countdown===1 || countdown===0 || countdown===45){
         setIsReady(true);
       }
